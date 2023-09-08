9177698fe34e4ec5710985cf46376d319c7c6249
zayyid123 <mochamadzayyid@gmail.com>
2023-09-08 19:39:30 +0700

    implement color

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='text-red-300'>Jemari semut</div>
+      <div className='bg-def-100'>Jemari semut</div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,6 +14,28 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    colors: {
+      'biru': {
+        100: '#C2ECF9',
+        200: '#47c1e7'
+      },
+      'merah': {
+        100: '#FFDEDA',
+        200: '#FF9283'
+      },
+      'hijau': {
+        100: '#F2F7E3',
+        200: '#B9DD53'
+      },
+      'ungu': {
+        100: '#ECE3FA',
+        200: '#BFA2EF'
+      },
+      'def': {
+        100: '#E0F1F5'
+      },
+      ...colors,
     },
   },
   plugins: [],
